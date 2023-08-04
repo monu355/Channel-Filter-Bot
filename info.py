e6aec3599b0718fbf9642f279702727b915034f0
leomemlon <139001721+monu875@users.noreply.github.com>
2023-08-04 21:35:23 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,6 +1,6 @@
-API_ID       = 16818625
-API_HASH     = "38fb061efb9e27b865f6923d2bb03e49"
-BOT_TOKEN    = "5186428454:AAHLxZSRZHDg3HZHNscrEwSVRvfgwdS_YtY"
+API_ID       = 16909652
+API_HASH     = "faa1e17a975a12f80488741488670189"
+BOT_TOKEN    = "5314330225:AAEVIQKbBky9poNAJn4pBP4vRayLxrhGGo0"
 SESSION      = ""
 DATABASE_URI = "mongodb+srv://prithviraj:chauhan@atlascluster.8wjoq8m.mongodb.net/?retryWrites=true&w=majority"
 LOG_CHANNEL  = -1001757089751
